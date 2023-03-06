69b713e1501c773c32fac414a6a58617ec510ac4
Ntare1 <isaacntare1@gmail.com>
2023-03-06 23:39:25 +0200

    slicing

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-<<<<<<< HEAD
-str = str.split("\\")
+str = str[39:67] + str[-22:-17] + str[0:6]
 print(str)
-=======
-str = str[39:66] + str[-23:-17] + str[:6]
-print(str)
->>>>>>> 16061e2c58ff2e93732cfbe03ea2ee71c32b639c
