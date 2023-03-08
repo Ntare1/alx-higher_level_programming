cda9bc27dc628225876bdadd4e80a595d10f358e
Ntare1 <isaacntare1@gmail.com>
2023-03-08 11:28:57 -0800

    functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,3 @@ for alp in range(97, 123):
     if alp == 101 or alp == 113:
         continue
     print("{}".format(chr(alp)), end="")
-    
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range(0,100):
+for x in range(0, 100):
     if x == 99:
         print("{:02}".format(x), end="")
     else:
