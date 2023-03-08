052607e1b3246ee4c6f8e73489668e22f9825f53
Ntare1 <isaacntare1@gmail.com>
2023-03-08 13:14:18 +0200

    functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(10):
-        if x == 9 and y == 9:
-            print("{}{}".format(x, y), end="")
-        else:
-            print("{}{}".format(x, y), end=", ")
+for x in range(0,100):
+    if x == 99:
+        print("{:02}".format(x), end="")
+    else:
+        print("{:02}".format(x), end=", ")
