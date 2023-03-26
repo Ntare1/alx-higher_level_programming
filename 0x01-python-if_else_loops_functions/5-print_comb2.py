ed30bf18269b91dc2956af0fb496362c0b655bdc
Ntare1 <isaacntare1@gmail.com>
2023-03-26 08:26:00 +0200

    Conditional statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for x in range(0, 100):
     if x == 99:
-        print("{:02}".format(x), end=" ")
+        print("{}".format(x))
     else:
         print("{:02}".format(x), end=", ")
+
