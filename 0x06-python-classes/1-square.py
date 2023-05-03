257e32e3d84fbf69ee35c93064985509875783d7
Ntare1 <isaacntare1@gmail.com>
2023-05-03 11:06:21 -0700

    Classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,6 +3,6 @@
 
 
 class Square:
-    """ Method that initializes the attributes of an object eg. sise is an an attribute"""
+    """ Method that initializes the attributes of an object """
     def __init__(self, size):
         self.__size = size
