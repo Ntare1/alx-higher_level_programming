574ef6e2ff462437fc79b9a268d643dbdc0a8df3
Ntare1 <isaacntare1@gmail.com>
2023-05-03 18:25:42 +0200

    Classes(initialization of a private attribute)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Class that defines a square """
+
+
+class Square:
+    """ Method that initializes the attributes of an object eg. sise is an an attribute"""
+    def __init__(self, size):
+        self.__size = size
