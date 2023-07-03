84f4107342888e6d73af3afecbfef3e014a365fe
Ntare1 <isaacntare1@gmail.com>
2023-07-03 21:01:14 +0200

    Tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,9 +19,9 @@ def add_integer(a, b=98):
     TypeError: in case an of the args is not an integer or float
 
     """
-    if not (isinstance(a, int)) and not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
             raise TypeError("a must be an integer")
-    if not not (isinstance(b, int)) and not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
             raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
