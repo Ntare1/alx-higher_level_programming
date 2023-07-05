e591bc4251f4a1897239b3ba38ccb022471f2cb6
Ntare1 <isaacntare1@gmail.com>
2023-07-05 12:54:30 -0700

    inheritance"

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Module containg a function that  returns the list of available 
+Module containg a function that  returns the list of available
 attributes and methods of an object
 """
 
 
 def lookup(obj):
-    """ 
-    Function returns a list of available 
-    attribute and methods of an object 
+    """
+    Function returns a list of available
+    attribute and methods of an object
     """
     return(dir(obj))
