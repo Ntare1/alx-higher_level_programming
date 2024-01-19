40a2f793fcb94550e23b8177e35abb6591094fbc
Ntare1 <isaacntare1@gmail.com>
2024-01-19 12:01:27 -0800

    Python import modules

--- 0x04-python-more_data_structures/102-complex_delete.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    keys = []
-    for x in a_dictionary:
-        if a_dictionary[x] == value:
-            keys.append(x)
-    for key in keys:
-        del a_dictionary[x]
-    return(a_dictionary)
